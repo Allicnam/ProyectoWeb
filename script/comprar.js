0e19b1b91f4fb16d141078a734b57c23025e7d17
RubenLCharles <33010291+RubenLCharles@users.noreply.github.com>
2020-11-22 20:11:03 -0600

    EmailJS

---
+++ script/comprar.js
@@ -0,0 +1,30 @@
+
+emailjs.init('user_1TK27ROnqf46mrzcvKJxQ');
+
+function sendEmail(){
+
+
+    let name = document.getElementById("userName").textContent;
+    let books = document.getElementById("bookName").textContent;
+    let cost = document.getElementById("totalCost");
+
+
+    emailjs.send("gmail","template_r0db7sg",{
+    from_name: name,
+    order: books,
+    total: cost,
+    });
+
+    alert("Orden realizada!");
+
+}
+
+var btnSubmit = document.getElementById("btnSubmit");
+btnSubmit.addEventListener(
+    "click",
+    function(){
+        sendEmail();
+    }
+    ,
+    false
+); 
